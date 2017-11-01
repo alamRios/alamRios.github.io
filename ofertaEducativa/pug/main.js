050852639a443520aeaa01cb61e0b533eae51428
FloranyelliA <floranyellianaya@outlook.com>
2017-10-31 19:54:32 -0600

    version final unificada

---
+++ ofertaEducativa/pug/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+$('.up').hide();
+$(window).scroll(function(){
+if($(this).scrollTop()>100){
+$('.up').fadeIn();
+}else{
+    $('.up').fadeOut();
+	}
+});
+$('.up').click(function(){
+$('body,html').animate({
+scrollTop:0
+});
+});
+});
